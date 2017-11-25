f789e475113a292171299499a7b2b948bedb658f
osamuhiraishi <osm890221@gmail.com>
2017-11-25 13:29:22 +0000

    secondlogin

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,20 +3,14 @@ class SessionsController < ApplicationController
   def new
   end
 
+  # POST /login 
   def create
     user = User.find_by(email: params[:session][:email])
     if user && user.authenticate(params[:session][:password])
-      if user.activated?
-        # Success
-        log_in user
-        params[:session][:remember_me] == '1' ? remember(user) : forget(user)
-        redirect_back_or user
-      else
-        message  = "Account not activated. "
-        message += "Check your email for the activation link."
-        flash[:warning] = message
-        redirect_to root_url
-      end
+      # Success
+      log_in user
+      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
+      redirect_back_or user
     else
       # Failure
       flash.now[:danger] = 'Invalid email/password combination'
